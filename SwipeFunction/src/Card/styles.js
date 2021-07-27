c1d28e68289397deb6045f9e7e5c9529e9a8da48
naz-az <87741419+naz-az@users.noreply.github.com>
2021-07-27 06:18:43 +0200

    Add files via upload

---
+++ SwipeFunction/src/Card/styles.js
@@ -0,0 +1,43 @@
+  
+import { StyleSheet } from 'react-native';
+import { CARD } from '../utils/constants';
+
+export const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 130,
+  },
+  image: {
+    width: 500,
+    height: 570,
+    borderRadius: CARD.BORDER_RADIUS,
+  },
+  gradient: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 160,
+    borderRadius: CARD.BORDER_RADIUS,
+  },
+  name: {
+    position: 'absolute',
+    bottom: 22,
+    left: 22,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  choiceContainer: {
+    position: 'absolute',
+    top: 100,
+  },
+  likeContainer: {
+    left: 45,
+    transform: [{ rotate: '-30deg' }],
+  },
+  nopeContainer: {
+    right: 45,
+    transform: [{ rotate: '30deg' }],
+  },
+});
---
+++ SwipeFunction/src/Main/data.js
@@ -0,0 +1,22 @@
+export const pets = [
+    {
+      name: 'Nike shoes',
+      source: require('../../assets/nikeshoes.jpg'),
+    },
+    {
+      name: 'MacBook Pro',
+      source: require('../../assets/mcbook.jpg'),
+    },
+    {
+      name: 'Lamborghini',
+      source: require('../../assets/Lamborghini.jpg'),
+    },
+    {
+      name: 'PlayStation 5',
+      source: require('../../assets/PS5.jpg'),
+    },
+    {
+      name: 'Iphone XS',
+      source: require('../../assets/iphone_xs.jpg'),
+    },
+  ];
