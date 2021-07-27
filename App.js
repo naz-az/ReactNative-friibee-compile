d9e51e8b907f6084af0da5d2991aaf897f070f17
naz-az <87741419+naz-az@users.noreply.github.com>
2021-07-27 06:17:56 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import 'react-native-gesture-handler';
+
+import Function from './bottom-tab-bar/Function';
+
+
+
+export default function App() {
+    return (
+      <>
+        <Function />
+        <StatusBar style="auto" />
+      </>
+    );
+  }
