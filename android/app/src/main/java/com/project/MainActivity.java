6cd239869cb853bfbf0126cfd7061682bb656950
naz-az <87741419+naz-az@users.noreply.github.com>
2021-08-08 03:11:35 +0200

    Add files via upload

--- android/app/src/main/java/com/project/MainActivity.java
+++ android/app/src/main/java/com/project/MainActivity.java
@@ -1,16 +1,8 @@
 package com.project;
 
 import com.facebook.react.ReactActivity;
-import android.os.Bundle;
-import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -18,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-     return "Project";
+    return "Project";
   }
 }
